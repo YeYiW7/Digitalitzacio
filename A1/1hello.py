3aa623b8c13e08061638ea9853ae69e0577b8650
YeYi <yeyi.w074@gmail.com>
2025-09-29 14:08:32 +0200

    vr2

---
+++ A1/1hello.py
@@ -0,0 +1,12 @@
+print("Hello word")
+a = 5
+b = 2.1
+s = "Hola"
+print(type(s))
+print(type(a))
+print(type(b))
+if a == 5:
+    print("Dentro IF")
+    print("Dentro IF 2")
+
+print("Fuera del IF")
---
+++ A1/2suma.py
@@ -0,0 +1,4 @@
+a=input("Dona'm un número:")
+b=input("Dona'm un altre número:")
+c= a + b
+print("La suma de" , a , "+", b , "=" , c)
