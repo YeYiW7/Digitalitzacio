d4926468e2c82f819d8526d60d24f19d00b1cf16
YeYi <yeyi.w074@gmail.com>
2025-10-06 14:58:26 +0200

    vr8

--- ExercicisPython/Ex7.py
+++ ExercicisPython/Ex7.py
@@ -1,1 +1,9 @@
-x="programació"
+## Ex 11
+contador= 0
+paraula="programació"
+lletra ='a'
+
+for x in paraula:
+    if x == lletra:
+        contador+=1
+print ("A la paraula ", paraula ,'hi ha ', contador, "Lletres:" , lletra) 
