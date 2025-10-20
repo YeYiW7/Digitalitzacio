7642bb611deb749e083fe5c38fa82ed42f6942ca
YeYi <yeyi.w074@gmail.com>
2025-10-20 14:56:16 +0200

    vr11

---
+++ Apunts1_2/llistes.py
@@ -0,0 +1,22 @@
+l=[1,2,3,5,7]
+print(l)
+l.append(11)
+print(l)
+l.pop(4)
+print(l)
+
+# Manera 1
+for x in l:
+    print(x)
+
+# Manera 2
+for x in range(len(l)):
+    print(l[x])
+
+#Manera 3
+cont= 0
+while cont < len(l):
+    print(l[cont])
+    cont+=1
+
+
---
+++ ExercicisPythonA2/Ex5.py
@@ -0,0 +1,6 @@
+llista_compra =['pomes','pa','oli','llet']
+for producte in range(len(llista_compra )):
+    print('He de comprar: ' + llista_compra [producte])
+
+#for x in llista_compra:
+#    print(x)
