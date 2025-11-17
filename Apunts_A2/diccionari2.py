9bb1c2f2b09926808e0318564e1599fc70b95a73
YeYi <yeyi.w074@gmail.com>
2025-11-17 14:53:53 +0100

    vr16

---
+++ Apunts_A2/diccionari2.py
@@ -0,0 +1,12 @@
+# Frase
+frase = '''
+Hola mundo, esto es una frase de prueba
+Hola mundo, esto es una frase de prueba
+Hola mundo, esto es una frase de prueba
+Hola mundo, esto es una frase de prueba
+'''
+
+
+# Usar split() por defecto (divide por espacios)
+palabras_espacios = frase.split()
+print(palabras_espacios)
