87fe3b882bf89c50ec682784a6ad1f7bc81e51ea
YeYi <yeyi.w074@gmail.com>
2025-12-15 14:55:37 +0100

    vr21

--- ExercicisPythonA2/A2_2/AC2_EXERCICIS/Ex1.py
+++ ExercicisPythonA2/A2_2/AC2_EXERCICIS/Ex1.py
@@ -1,1 +1,18 @@
-cotxe=
+# 2.1 Crear el diccionari
+cotxe = {
+    "marca": "Toyota",
+    "model": "Corolla",
+    "any": 2020
+}
+
+# Mostrar el valor de la clau "marca"
+print(cotxe["marca"])
+
+# 2.2 Afegir una nova clau
+cotxe["color"] = "vermell"
+
+# Eliminar la clau "any"
+del cotxe["any"]
+
+# Mostrar el diccionari actualitzat
+print(cotxe)
